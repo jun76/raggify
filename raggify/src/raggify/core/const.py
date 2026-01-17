7378a62684e94e35b69ac405196da11deb51434f
jun76 <jun76.git@gmail.com>
2026-01-17 15:07:55 +0000

    oops

--- raggify/src/raggify/core/const.py
+++ raggify/src/raggify/core/const.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 PROJECT_NAME: str = "raggify"
 PJNAME_ALIAS: str = "rg"
-VERSION: str = "0.3.1"
+VERSION: str = "0.3.3"
 DEFAULT_CONFIG_PATH: str = f"/etc/{PROJECT_NAME}/config.yaml"
 DEFAULT_KNOWLEDGEBASE_NAME: str = "default_kb"
 DEFAULT_WORKSPACE_PATH: Path = Path.home() / ".local" / "share" / PROJECT_NAME
